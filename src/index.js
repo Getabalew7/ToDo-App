2754a6468d4431a57f44ef7f6f2b59e24046aa9d
Getabalew <getabalewamtate@gmail.com>
2021-02-15 16:19:08 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 //component
 import TodoContainer from './functionBased/components/TodoContainer'
 //stylesheet
@@ -8,8 +8,8 @@ import "./functionBased/App.css"
 
 ReactDOM.render(
    <React.StrictMode>
-      <BrowserRouter>
+      <Router basename={process.env.PUBLIC_URL}>
          <TodoContainer />
-      </BrowserRouter>
+      </Router>
    </React.StrictMode>,
    document.getElementById('root'))
